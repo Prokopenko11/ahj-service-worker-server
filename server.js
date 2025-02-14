2671be1076dada92cd6e6a617953c448ec54734c
Valeria Prokopenko <valeriaprokopenko59@gmail.com>
2025-02-14 17:24:53 +0300

    server.js changed

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.use(async (ctx) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Сервер запущен на http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
